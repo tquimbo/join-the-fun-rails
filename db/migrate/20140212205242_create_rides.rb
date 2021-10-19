a21ce6bc5ac1d9e4c07266ec497b90dd8c7fc97c
Tim Quimbo <tquimbo1@gmail.com>
2021-10-18 20:07:07 -0400

    commit tree a656dce14771617db872226ac78494c2269a1f4a

--- db/migrate/20140212205242_create_rides.rb
+++
@@ -1,7 +0,0 @@
-class CreateRides < ActiveRecord::Migration[5.0]
-  def change
-    create_table :rides do |t|
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20140212205318_create_passengers.rb
+++
@@ -1,7 +0,0 @@
-class CreatePassengers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :passengers do |t|
-      t.timestamps null: false
-    end
-  end
-end
